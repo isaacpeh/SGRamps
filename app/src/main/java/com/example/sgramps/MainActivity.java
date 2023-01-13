ae364b97d7a48de3b75179af264445a355d1c1a8
isaacpeh <isaacpeh123@gmail.com>
2023-01-13 15:55:13 +0800

    WIP : Working on navbar

--- app/src/main/java/com/example/sgramps/MainActivity.java
+++ app/src/main/java/com/example/sgramps/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.sgramps;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        NavigationBarView bottomNav = findViewById(R.id.bottom_navigation_view);
+        bottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home_page:
+                        Toast.makeText(getApplicationContext(),"Home",Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.bookmarks_page:
+                        Toast.makeText(getApplicationContext(),"Saved",Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.upload_page:
+                        Toast.makeText(getApplicationContext(),"Upload",Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.notification_page:
+                        Toast.makeText(getApplicationContext(),"Updates",Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.profile_page:
+                        Toast.makeText(getApplicationContext(),"Profile",Toast.LENGTH_SHORT).show();
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
     }
 }
