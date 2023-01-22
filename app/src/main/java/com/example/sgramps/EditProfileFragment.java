641856b63793002fe9bb3338cda4b2b251645cd3
Isaac <ISAACPEH.21@ichat.sp.edu.sg>
2023-01-23 03:26:23 +0800

    WIP : Working on edit profile

--- app/src/main/java/com/example/sgramps/EditProfileFragment.java
+++ app/src/main/java/com/example/sgramps/EditProfileFragment.java
@@ -9,9 +9,16 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class EditProfileFragment extends Fragment implements View.OnClickListener {
+    String[] gender = {"Male", "Female"};
+    AutoCompleteTextView autoCompleteTextView;
+    ArrayAdapter<String> arrayAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -19,6 +26,10 @@ public class EditProfileFragment extends Fragment implements View.OnClickListene
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_edit_profile, container, false);
 
+        autoCompleteTextView = view.findViewById(R.id.ddlGender);
+        arrayAdapter = new ArrayAdapter<>(getActivity(), R.layout.dropdown_item, gender);
+        autoCompleteTextView.setAdapter(arrayAdapter);
+        
         // This callback will only be called when MyFragment is at least Started.
         OnBackPressedCallback callback = new OnBackPressedCallback(true /* enabled by default */) {
             @Override
