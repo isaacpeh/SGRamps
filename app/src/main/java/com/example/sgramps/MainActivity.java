d7fd60367b03a2ac45c20d10cbb51b230bd40f89
barry <leebarry008@gmail.com>
2023-01-27 04:00:25 +0800

    firebase authentication, design WIP

--- app/src/main/java/com/example/sgramps/MainActivity.java
+++ app/src/main/java/com/example/sgramps/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
 
+    FirebaseAuth fAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fAuth = FirebaseAuth.getInstance();
+
     }
 }
